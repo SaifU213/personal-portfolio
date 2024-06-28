10f711857cb260ccd7293062de5c05a83f7c6037
Saif <saifuddin@live.ca>
2024-06-28 19:56:11 -0400

    Unoptimized imaged

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -49,7 +49,7 @@ const ProjectsSection = () => {
                 <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
                   <div className="md:w-1/2">
                     <Link href={project.link}>
-                      <Image
+                      <Image unoptimized
                         src={project.image}
                         alt=""
                         width={1000}
