38fe0ed49d816abf499e0f5a3ef0e65e77caaf85
Saif <saifuddin@live.ca>
2024-06-28 22:07:43 -0400

    Paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const isProd = process.env.NODE_ENV === 'production';
+
+console.log(`isProd: ${isProd}`); // Add this line to log the value of isProd
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`); // Add this line to log the value of NODE_ENV
 
 const nextConfig = {
   basePath: isProd ? "/personal-portfolio" : "",
@@ -8,7 +12,7 @@ const nextConfig = {
   distDir: "dist",
   images: {
     unoptimized: true,
-    path: isProd ? '/personal-portfolio/' : '/',
+    path: isProd ? 'https://saifu213.github.io/personal-portfolio/' : '/',
   },
 };
 
