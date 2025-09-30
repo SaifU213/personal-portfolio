1f525087af1200160abfbfb876c1fbbfeaadedd5
SaidU213 <saifuddin@live.ca>
2025-09-30 16:33:49 -0400

    Update

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,62 +1,66 @@
-"use client";
+'use client';
 
-import React from "react";
-import Image from "next/image";
-import { Link } from "react-scroll/modules";
-import { HiArrowDown } from "react-icons/hi";
+import React from 'react';
+import Image from 'next/image';
+import { Link } from 'react-scroll/modules';
+import { HiArrowDown } from 'react-icons/hi';
 
 const HeroSection = () => {
-  return (
-    <section id="home">
-      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
-        <div className="md:mt-2 md:w-1/2">
-          <Image 
-            className="rounded-full shadow-2xl"
-            src={`${process.env.NEXT_PUBLIC_BASE_PATH}/headshot.png`}
-            alt=""
-            width={325}
-            height={325}
-          />
-        </div>
-        <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">
-            Hi, I&apos;m Saif
-          </h1>
-          <p className="text-lg mt-4 mb-6 md:text-2xl">
-            I&apos;m a{" "}
-            <span className="font-semibold text-purple-700">
-              Software Engineer{" "}
-            </span>
-            based in Toronto, ON. Aspiring to create software that makes life
-            easier and more meaningful.
-          </p>
-          <Link
-            to="projects"
-            className="text-neutral-100 font-semibold px-6 py-3 bg-purple-700 rounded shadow hover:bg-purple-800"
-            activeClass="active"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}
-          >
-            Projects
-          </Link>
-        </div>
-      </div>
-      <div className="flex flex-row items-center text-center justify-center ">
-        <Link
-          to="about"
-          activeClass="active"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-        >
-          <HiArrowDown size={35} className="animate-bounce" />
-        </Link>
-      </div>
-    </section>
-  );
+	return (
+		<section id="home">
+			<div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 pt-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
+				<div className="md:mt-2 md:w-1/2">
+					<Image
+						className="rounded-full shadow-2xl"
+						src={`${process.env.NEXT_PUBLIC_BASE_PATH}/headshot.png`}
+						alt=""
+						width={325}
+						height={325}
+					/>
+				</div>
+				<div className="md:mt-2 md:w-3/5">
+					<h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">
+						Hi, I&apos;m Saif
+					</h1>
+					<p className="text-lg mt-4 mb-6 md:text-2xl">
+						I&apos;m a{' '}
+						<span className="font-semibold text-purple-700">
+							Computer Science Student{' '}
+						</span>{' '}
+						and a{' '}
+						<span className="font-semibold text-purple-700">
+							Software Engineer{' '}
+						</span>
+						based in Toronto, ON. Aspiring to create software that makes life
+						easier and more meaningful.
+					</p>
+					<Link
+						to="projects"
+						className="text-neutral-100 font-semibold px-6 py-3 bg-purple-700 rounded shadow hover:bg-purple-800"
+						activeClass="active"
+						spy={true}
+						smooth={true}
+						offset={-100}
+						duration={500}
+					>
+						Projects
+					</Link>
+				</div>
+			</div>
+			<div className="flex flex-row items-center text-center justify-center ">
+				<Link
+					to="about"
+					activeClass="active"
+					spy={true}
+					smooth={true}
+					offset={-100}
+					duration={500}
+				>
+					<HiArrowDown size={35} className="animate-bounce" />
+				</Link>
+			</div>
+		</section>
+	);
 };
 
 export default HeroSection;
